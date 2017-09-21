ed3a5431f9ba6da10f6b974b2a9adfb45684ef45
Kushan Patel <kushanpatelcmu@gmail.com>
2017-09-21 14:44:58 -0700

    adding code to do some wiring

--- blog/src/main/webapp/src/js/app.js
+++ blog/src/main/webapp/src/js/app.js
@@ -1,20 +1,25 @@
 var blog = angular.module("blog", [
     "ngRoute",
-    "blog.controllers"
+    "blog.controllers",
+    "blog.services"
 ]);
 
 blog.config(["$routeProvider", "$locationProvider", "$sceDelegateProvider",
     function($routeProvider, $locationProvider, $sceDelegateProvider) {
         $locationProvider.hashPrefix('');
         $sceDelegateProvider.resourceUrlWhitelist(['self', new RegExp('^(http[s]?):\/\/(w{3}.)?youtube\.com/.+$')]);
-//        $routeProvider.
-//            when("/home", {
-//                templateUrl: "src/html/home.html",
-//                controller: 'homeCtrl',
-//            }).
-//            when("/", {
-//                redirectTo: "/home"
-//            })
+        $routeProvider.
+            when("/mgmt", {
+                templateUrl: "src/html/management.html",
+                controller: 'mgmtCtrl'
+            }).
+            when("/home", {
+                templateUrl: "src/html/home.html",
+//                controller: 'mainCtrl'
+            }).
+            when("/", {
+                redirectTo: "/home"
+            })
     }
 ]);
 
---
+++ blog/src/main/webapp/src/js/services.js
@@ -0,0 +1,6 @@
+angular.module("blog.services", [])
+    .service('DataService', function($scope, $rootScope, $route, $routeParams, $http) {
+
+
+    }
+);
